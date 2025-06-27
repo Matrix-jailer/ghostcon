98ac1c7cfb0f62304ebd08b66ca653f733e84394
Matrix <66746496+Matrix-jailer@users.noreply.github.com>
2025-06-27 16:16:10 +0500

    Update GhostAPIPRO.py

--- GhostAPIPRO.py
+++ GhostAPIPRO.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.common.exceptions import WebDriverException
 from seleniumwire import webdriver
 from urllib.parse import urljoin
